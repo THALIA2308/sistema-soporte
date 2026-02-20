538da90f32bc446441fd78116904225e465af9ca
thalia alarcon <th.alarcon@duocuc.cl>
2026-02-20 12:12:58 -0300

    arreglos

--- soporte/urls.py
+++ soporte/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from tickets.views import crear_ticket, ticket_exito
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', crear_ticket, name='crear_ticket'),
     path('exito/', ticket_exito, name='ticket_exito'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
